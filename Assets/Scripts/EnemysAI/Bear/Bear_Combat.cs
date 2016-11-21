e4c0d16cd0555d71221391ef58fe54c283600470
batomow <carlosa_miranda@outlook.com>
2016-11-20 18:29:03 -0600

    Ya ando termiando el rework de los enemigos.

---
+++ Assets/Scripts/EnemysAI/Bear/Bear_Combat.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bear_Combat : MonoBehaviour, IDamagable {
+
+	private Bear_Health myHealth;
+	private Rigidbody2D myRigid; 
+	private Transform player; 
+	private RaycastHit2D front, back; 
+	private Vector2 direction; 
+	private float DistanceFromPlayer = 0, t, force; 
+	public AnimationCurve SpeedFunction; 
+	public float Speed; 
+
+
+
+	void Start () {
+		myHealth = this.gameObject.GetComponent<Bear_Health> (); 
+		myRigid = this.gameObject.GetComponent<Rigidbody2D> (); 
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>(); 
+		Bear_Patrol.FoundPlayer += combat_start; 
+	}
+
+	IEnumerator CheckPlayerDirection(){
+		if ((player.position.x - transform.position.x) < 0) {
+			direction = -transform.right; 
+			StartCoroutine (TurnAround ()); 
+		} else{
+			direction = transform.right; 
+			StartCoroutine (CheckDistanceFromPlayer ()); 
+		} 
+		yield return null; 
+	}
+	IEnumerator TurnAround(){
+		for (int n = 0; n < 18; n++) {
+			transform.Rotate (Vector2.up, 10f); 
+			yield return new WaitForSeconds (2*Time.fixedDeltaTime);
+		}
+		yield return StartCoroutine(CheckDistanceFromPlayer()); 
+	}
+	IEnumerator CheckDistanceFromPlayer(){
+		DistanceFromPlayer = player.position.x - transform.position.x; 
+		if (DistanceFromPlayer < 2f) {
+			StartCoroutine (Attack ()); 
+		} else {
+			StartCoroutine (GetClose ()); 
+		}
+		yield return null; 
+	}
+	IEnumerator GetClose(){
+		if (t >= 1) {
+			t = 1; 
+		} else {
+			t += Time.fixedDeltaTime; 
+		}
+		//calculate force
+		force = (Mathf.Pow((SpeedFunction.Evaluate(t)*Speed),2)/2f); 
+		//apply force 
+		if (myRigid.velocity.magnitude < Speed / 2) {
+			myRigid.AddForce (direction * force); 
+		}
+		yield return new WaitForSeconds (0.25f); 
+		yield return StartCoroutine (CheckPlayerDirection());
+	}
+	IEnumerator Attack(){
+		//Do some damage to player. 
+		Debug.Log("Hitting Player"); 
+		yield return StartCoroutine(CheckPlayerDirection()); 
+	}
+
+	public void combat_stop(){
+		myRigid.velocity = Vector2.zero; 
+		StopAllCoroutines (); 
+	}
+	public void combat_start(){
+		StartCoroutine (CheckPlayerDirection ()); 
+	}
+
+	public void DamageMe(float Damage, Vector2 force){
+		//lower this objects health and apply nock back. 
+		myRigid.AddForce(force); 
+		myHealth.Health-=Damage; 
+	}
+	public void DamageMe(Vector2 force){
+		//calculate damage. 
+		float Damage = force.magnitude; 
+		//lower health and apply nockback.  
+		myRigid.AddForce(force); 
+		myHealth.Health-=Damage; 
+	}
+
+}
---
+++ Assets/Scripts/EnemysAI/Bear/Bear_Patrol.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bear_Patrol : MonoBehaviour {
+
+	private Rigidbody2D myRigid; 
+	private RaycastHit2D eyeLevel, feet; 
+	public AnimationCurve SpeedFunction; 
+	public float Speed; 
+	private float t, force; 
+
+	public delegate void PatrolEvents (); 
+	public static event PatrolEvents FoundPlayer; 
+
+
+	void Start () {
+		myRigid = this.gameObject.GetComponent<Rigidbody2D> (); 
+		StartCoroutine (CheckForObstacles ()); 
+	}
+
+	IEnumerator CheckForObstacles(){
+		//check if there is an obstacle infront of you (a gap or a wall)
+		eyeLevel = Physics2D.Raycast(transform.position, transform.right, 3f); 
+		feet = Physics2D.Raycast (transform.position, new Vector2(transform.right.x, -0.4f).normalized, 3f); 
+		Debug.DrawLine (transform.position, transform.position + (3 * transform.right), Color.red); 
+		Debug.DrawLine (transform.position, transform.position + (3 * new Vector3(transform.right.x, -0.4f).normalized), Color.red); 
+		if (feet.collider == null) {
+			StartCoroutine (TurnAround ()); 
+		} else if(feet.collider!=null&&eyeLevel.collider!=null){
+			if(feet.collider.tag!="Player" && eyeLevel.collider.tag != "Player"){
+				StartCoroutine (TurnAround());
+			}
+		}else{
+			StartCoroutine (Move ()); 
+		}
+		yield return null; 
+
+	}
+	IEnumerator TurnAround(){
+		//stop all movemnet. 
+		myRigid.velocity = Vector2.zero; 
+		t = 0; 
+		//turn 180 degreees. 
+		for (int n = 0; n < 18; n++) {
+			transform.Rotate (Vector2.up, 10f); 
+			yield return new WaitForSeconds (2*Time.fixedDeltaTime);
+		}
+		yield return StartCoroutine(CheckForObstacles()); 
+	}
+	IEnumerator Move(){
+		if (t >= 1) {
+			t = 1; 
+		} else {
+			t += Time.fixedDeltaTime; 
+		}
+		//calculate force
+		force = (Mathf.Pow((SpeedFunction.Evaluate(t)*Speed),2)/2f); 
+		//apply force 
+		if (myRigid.velocity.magnitude < Speed / 2) {
+			myRigid.AddForce (transform.right * force); 
+		}
+		yield return new WaitForSeconds (0.25f); 
+		yield return StartCoroutine (CheckForObstacles());
+	}
+
+
+	public void patrol_stop(){
+		myRigid.velocity = Vector2.zero; 
+		t = 0; 
+		StopAllCoroutines (); 
+	}
+	public void patrol_start(){
+		StartCoroutine(CheckForObstacles()); 
+	}
+
+	void OnTriggerEnter2D(Collider2D something){
+		if (something.tag == "Player") {
+			patrol_stop (); 
+			FoundPlayer (); 
+			Debug.Log ("Found Player"); 
+		}
+	}
+	
+
+}
