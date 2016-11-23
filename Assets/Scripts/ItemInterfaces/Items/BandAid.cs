5c170a0445d87205fb3ae12f3b49315a8c174282
julio177 <julioc.aguilarv@gmail.com>
2016-11-23 14:12:10 -0600

    crea interfaces e items a utilizar en el juego, pendiente implementacion concreta

---
+++ Assets/Scripts/ItemInterfaces/Items/BandAid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BandAid : MonoBehaviour, ICollectable, IConsumable, IDetectable {
+	public void TakeItem () {
+
+	}
+
+	public void Consume () {
+
+	}
+
+	public void Detect () {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ItemInterfaces/Items/Sword.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : MonoBehaviour, IEquippable {
+
+	public void Equip () {
+
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
