b65c3698cd801ddf5d24d77f0124fdad2f12b5b3
Jorge Paredes <jopar219@gmail.com>
2016-11-23 11:53:39 -0600

    Make twoDFy use transform instead of rigidbody

--- Assets/Scripts/twoDFy.cs
+++ Assets/Scripts/twoDFy.cs
@@ -4,17 +4,17 @@ using System.Collections;
 [ExecuteInEditMode]
 public class twoDFy : MonoBehaviour {
 
-	[SerializeField] Rigidbody2D counterpart;
+	[SerializeField] Transform counterpart;
 	[SerializeField] BezierSpline ground;
 	// Use this for initialization
 	void Start () {
-		transform.position = ground.GetPoint(counterpart.transform.position.x/100+0.5f)+Vector3.up*counterpart.transform.position.y;
-		transform.rotation = Quaternion.LookRotation(ground.GetDirection(counterpart.transform.position.x / 100 + 0.5f));
+		transform.position = ground.GetPoint(counterpart.position.x/100+0.5f)+Vector3.up*counterpart.position.y;
+		transform.rotation = Quaternion.LookRotation(ground.GetDirection(counterpart.position.x / 100 + 0.5f));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = ground.GetPoint(counterpart.transform.position.x/100+0.5f)+Vector3.up*counterpart.transform.position.y;
-		transform.rotation = Quaternion.LookRotation(Vector3.Cross(ground.GetDirection(counterpart.transform.position.x / 100 + 0.5f), Vector3.up))*counterpart.transform.rotation;
+		transform.position = ground.GetPoint(counterpart.position.x/100+0.5f)+Vector3.up*counterpart.position.y;
+		transform.rotation = Quaternion.LookRotation(Vector3.Cross(ground.GetDirection(counterpart.position.x / 100 + 0.5f), Vector3.up))*counterpart.rotation;
 	}
 }
