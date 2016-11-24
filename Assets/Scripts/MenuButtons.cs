ea8fc79d163918a81ab5b94f86ada1bed9d4f584
julio177 <julioc.aguilarv@gmail.com>
2016-11-24 14:35:40 -0600

    agrega funcionalidad a buttons

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+	public void StartGame() {
+		SceneManager.LoadScene("NewFirstLevel");
+	}
+
+	public void MainMenu() {
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void Credits() {
+		SceneManager.LoadScene("Credits");
+	}
+}
